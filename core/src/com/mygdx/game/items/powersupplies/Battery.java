e8ab79a4fc7937e65f18be9ada05f01aa9e40df9
TemyHoi <teamych09@gmail.com>
2023-12-10 14:18:06 +0300

    Добавленны характерисктика

--- core/src/com/mygdx/game/items/powersupplies/Battery.java
+++ core/src/com/mygdx/game/items/powersupplies/Battery.java
@@ -1,4 +1,9 @@
 package com.mygdx.game.items.powersupplies;
 
 public class Battery extends PowerSupplies{
+    Battery(){
+        title="Батарейка ";
+        description="Вы родились с ней";
+        ability="вы получаете гипер заряд на 2 секунды. Гипер заряд ускоряет вас на 50% и увеличивает урон на 25%";
+    }
 }
--- core/src/com/mygdx/game/items/powersupplies/Orange.java
+++ core/src/com/mygdx/game/items/powersupplies/Orange.java
@@ -1,4 +1,10 @@
 package com.mygdx.game.items.powersupplies;
 
 public class Orange extends PowerSupplies{
+    Orange(){
+        title="Апельсин";
+        description="Кормить врагов звучит как крутая идея";
+        ability="Кормит половину врагов в комнате и накладывает эффект оглушения";
+    }
 }
+
