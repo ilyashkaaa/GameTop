bb79dbc0d22058f4df7716d813f8944fbc9aeadf
ilyashkaaa <ilyasuper50550@gmail.com>
2023-12-10 09:06:04 +0300

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,37 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+public class MyGdxGame extends ApplicationAdapter {
+	SpriteBatch batch;
+	Texture hero;
+	int SCR_WIDTH = 1280;
+	int SCR_HEIGHT = 720;
+	int heroX, heroY;
+	int speedHero = 5;
+	
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		hero = new Texture("hero.png");
+	}
+
+	@Override
+	public void render () {
+		ScreenUtils.clear(1, 2, 0, 1);
+		batch.begin();
+
+
+		batch.end();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+		hero.dispose();
+	}
+}
