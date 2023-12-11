f494e1b893b63326120b5dd0ac3bf84923fc67ab
ilyashkaaa <ilyasuper50550@gmail.com>
2023-12-11 14:19:07 +0300

    Добавлены классы врагов

---
+++ core/src/com/mygdx/game/enemies/Enemies.java
@@ -0,0 +1,11 @@
+package com.mygdx.game.enemies;
+
+
+public class Enemies {
+    protected String title;
+    protected String description;
+    int hp;
+    int damage;
+    int speed;
+
+}
---
+++ core/src/com/mygdx/game/enemies/Small.java
@@ -0,0 +1,9 @@
+package com.mygdx.game.enemies;
+
+
+public class Small extends Enemies {
+    public Small() {
+        title = "Мелкий пузырь";
+        description = "Очень быстрая скорость, очень мало хп, средний урон. Пытается влететь в игрока и взорваться";
+    }
+}
