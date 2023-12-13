18b516b2ac723d6799982edabcfa89c612bfd719
TemyHoi <teamych09@gmail.com>
2023-12-13 09:08:18 +0300

    врагу добавленна тектура

--- core/src/com/mygdx/game/enemies/Basic.java
+++ core/src/com/mygdx/game/enemies/Basic.java
@@ -1,9 +1,13 @@
 package com.mygdx.game.enemies;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 public class Basic extends Enemies {
     public Basic() {
         title = "Обычный пузырь";
         description = "Средняя скорость, среднее хп, средний урон. Просто идет в рукопашку.";
+        sprite = new Sprite(new Texture("textures/enemies/melee_city.png"));
     }
 
 }
