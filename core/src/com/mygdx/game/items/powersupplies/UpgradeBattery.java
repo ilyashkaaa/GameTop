3dc451f0618f1560b23f366bdc7578e83bf05e52
TemyHoi <teamych09@gmail.com>
2023-12-14 10:51:28 +0300

    Добавленно мини описание

--- core/src/com/mygdx/game/items/powersupplies/UpgradeBattery.java
+++ core/src/com/mygdx/game/items/powersupplies/UpgradeBattery.java
@@ -8,5 +8,7 @@ public class UpgradeBattery extends PowerSupplies {
         description = "Ваша батарейка, но без надписи с Maid in China";
         ability = "Вы получаете гипер заряд на 4 секунды. Гипер заряд ускоряет вас на 50% и увеличивает урон на 30%";
         texture = new Texture("texture/energy_sources/double_battery.png");
+        speedUp=0.5;
+        damageUp=1.3;
     }
 }
